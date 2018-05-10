e3598e69faf70af7f295d5560ac648943236920b
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-05-10 18:23:35 +0200

    new quote allows to create new quotes.

---
+++ src/components/Newquote.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row">
+    <form>
+          <div class="form-group">
+            <label>Quote
+              <textarea class="form-control rounded-0" rows="10" v-model="quote"></textarea>
+            </label>
+          </div>
+      <div class="col-md-8 col-md-6">
+        <button class="btn btn-primary" @click.prevent="create">Add Quote</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Newquote",
+    data:function () {
+      return {
+        quote:''
+      }
+    },
+    methods:{
+      create(){
+        this.$emit('oncreation', this.quote);
+        this.quote = '';
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
