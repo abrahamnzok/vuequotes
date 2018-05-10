ff0792eb390f8960c21043519172a7f7dee09f7e
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-05-10 16:29:07 +0200

    adding the Quotegrid manager to the component root element.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,34 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
+    <QuoteGrid :quotes="quotes"/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import QuoteGrid from "./components/QuoteGrid";
+
+  export default {
+    name: 'App',
+    components: {QuoteGrid},
+    data: function () {
+      return {
+        quotes: ['No tablecloths, silver cutlery, fine porcelain, sommeliers, or deep wine lists - that\'s fine. But no service or hospitality? That\'s going too far\n'],
+        limit: 10,
+        info: "You will need to only click on the quote to delete it"
+      }
+    }
+  }
 </script>
 
 <style>
-  @import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "../node_modules/bootstrap/dist/css/bootstrap-grid.min.css";
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
