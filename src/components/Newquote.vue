6269943ec63764f1005c27c10e8f26d63ee9c3a2
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-05-10 18:59:50 +0200

    small change to the html element.

--- src/components/Newquote.vue
+++ src/components/Newquote.vue
@@ -2,9 +2,8 @@
   <div class="row">
     <form>
           <div class="form-group">
-            <label>Quote
+            <label>Quote</label>
               <textarea class="form-control rounded-0" rows="10" v-model="quote"></textarea>
-            </label>
           </div>
       <div class="col-md-8 col-md-6">
         <button class="btn btn-primary" @click.prevent="create">Add Quote</button>
