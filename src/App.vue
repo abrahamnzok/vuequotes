6934cbdab1e2c641a0dab412b6016a3688b954fb
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-05-10 18:26:02 +0200

    added methods to create and deleteQuotes. I also added a info alert box.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
   <div id="app" class="container">
-    <QuoteGrid :quotes="quotes"/>
+    <div class="col-lg-12">
+      <newquote @oncreation="createQuote"/>
+    </div>
+    <QuoteGrid :quotes="quotes" @quoteDeleted="deleteQuotes"/>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          {{info}}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import QuoteGrid from "./components/QuoteGrid";
+  import Newquote from "./components/Newquote";
 
   export default {
     name: 'App',
-    components: {QuoteGrid},
+    components: {Newquote, QuoteGrid},
     data: function () {
       return {
         quotes: ['No tablecloths, silver cutlery, fine porcelain, sommeliers, or deep wine lists - that\'s fine. But no service or hospitality? That\'s going too far\n'],
         limit: 10,
-        info: "You will need to only click on the quote to delete it"
+        info: "You will only need to  click on the quote to delete it"
+      }
+    },
+    methods: {
+      createQuote(quote) {
+        this.quotes.push(quote);
+      },
+      deleteQuotes(index) {
+        this.quotes.splice(index, 1);
       }
     }
   }
 </script>
 
 <style>
-  @import "../node_modules/bootstrap/dist/css/bootstrap-grid.min.css";
+  @import '../node_modules/bootstrap/dist/css/bootstrap.css';
+  @import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
 
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
