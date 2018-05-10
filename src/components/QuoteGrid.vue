4e86646c3a0b1dae9ffbe2ba89fb0ed03d807aa3
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-05-10 16:27:58 +0200

    The quote grid manager first implementation

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <quote v-for="x in quotes">{{x}}</quote>
+  </div>
+</template>
+
+<script>
+  import Quote from "./Quote";
+  export default {
+    name: "QuoteGrid",
+    components: {Quote},
+    props:{
+      quotes:{
+        type:Array
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
